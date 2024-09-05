1d9223fc6df056e992f927b44f5d95f6a7faf807
ismoilnuriddinov <umidafayziyeva89@gmail.com>
2024-09-05 14:08:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " juft son");
+  } else {
+    console.log(i + " toq son");
+  }
+}
+
+// uyga vazifa:
+
+let sonlar = [2, 4, 9, 10, 45, 67, 8, 90];
+
+let juftSon = [];
+let toqSon = [];
+
+for (let i = 0; i < sonlar.length; i++) {
+  console.log(sonlar[i]);
+}
